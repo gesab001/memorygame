fed855452fc84fa00ab3cdb7454ce8f941f9bcd4
gesab001 <144000giovanni@gmail.com>
2020-08-11 00:40:55 +1200

    First commit

---
+++ app.js
@@ -0,0 +1,123 @@
+(function () {
+
+  var Memory = {
+
+    init: function (cards) {
+      this.$game = $(".game");
+      this.$modal = $(".modal");
+      this.$overlay = $(".modal-overlay");
+      this.$restartButton = $("button.restart");
+      this.cardsArray = $.merge(cards, cards);
+      this.shuffleCards(this.cardsArray);
+      this.setup();
+      this.binding();
+    },
+
+    shuffleCards: function (cardsArray) {
+      this.$cards = $(this.shuffle(this.cardsArray));
+    },
+
+    setup: function () {
+      this.html = this.buildHTML();
+      this.$game.html(this.html);
+      this.$memoryCards = $(".card");
+      this.paused = false;
+      this.guess = null;
+    },
+
+    binding: function () {
+      this.$memoryCards.on("click", this.cardClicked);
+      this.$restartButton.on("click", $.proxy(this.reset, this));
+    },
+    // kinda messy but hey
+    cardClicked: function () {
+      var _ = Memory;
+      var $card = $(this);
+      if (!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")) {
+        $card.find(".inside").addClass("picked");
+        if (!_.guess) {
+          _.guess = $(this).attr("data-id");
+        } else if (_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")) {
+          $(".picked").addClass("matched");
+          _.guess = null;
+        } else {
+          _.guess = null;
+          _.paused = true;
+          setTimeout(function () {
+            $(".picked").removeClass("picked");
+            Memory.paused = false;
+          }, 600);
+        }
+        if ($(".matched").length == $(".card").length) {
+          _.win();
+        }
+      }
+    },
+
+    win: function () {
+      this.paused = true;
+      setTimeout(function () {
+        Memory.showModal();
+        Memory.$game.fadeOut();
+      }, 1000);
+    },
+
+    showModal: function () {
+      this.$overlay.show();
+      this.$modal.fadeIn("slow");
+    },
+
+    hideModal: function () {
+      this.$overlay.hide();
+      this.$modal.hide();
+    },
+
+    reset: function () {
+      this.hideModal();
+      this.shuffleCards(this.cardsArray);
+      this.setup();
+      this.$game.show("slow");
+    },
+
+    // Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+    shuffle: function (array) {
+      var counter = array.length,temp,index;
+      // While there are elements in the array
+      while (counter > 0) {if (window.CP.shouldStopExecution(0)) break;
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+        // Decrease counter by 1
+        counter--;
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+      }window.CP.exitedLoop(0);
+      return array;
+    },
+
+    buildHTML: function () {
+      var frag = '';
+      this.$cards.each(function (k, v) {
+        frag += '<div class="card" data-id="' + v.id + '"><div class="inside">\
+				<div class="front"><img src="' + v.img + '"\
+				alt="' + v.name + '" /></div>\
+				<div class="back"><img src="https://gesab001.github.io/assets/images/sunset.jpg"\
+				alt="Codepen" /></div></div>\
+				</div>';
+      });
+      return frag;
+    } };
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const wordliststring = urlParams.get('imagelist');
+  var jsonobject = JSON.parse(wordliststring);
+  var cards = JSON.parse(jsonobject).items;
+
+
+  Memory.init(cards);
+
+
+})();
+//# sourceURL=pen.js
